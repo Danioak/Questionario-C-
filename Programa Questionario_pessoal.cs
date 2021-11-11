5e78e6965a3fb376351f35061541e1cc655eb032
DANI <93198583+Danisouzax@users.noreply.github.com>
2021-11-11 16:40:19 -0300

    Add files via upload

---
+++ Programa Questionario_pessoal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Questionario_pessoal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Pessoa!");
+            Console.WriteLine("Bem-Vindo ao questionário pessoal");
+            Console.Write("Qual seu nome?");
+            Console.Write("Bem Vindo, {0} \r\n " , Console.ReadLine());
+
+            Console.Write(" Qual sua idade? \r\n");
+
+            Console.Write("A sua idade é: {0} \r\n ", Console.ReadLine());
+
+            Console.Write("Homem ou Mulher?");
+            { 
+                
+
+            }
+           
+            
+ 
+        }
+    };
+}
